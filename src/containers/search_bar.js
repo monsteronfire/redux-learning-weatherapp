450ac89f3d0a225266d4cd435a6016615cdabfde
Shaii Ong <monsteronfire@gmail.com>
2017-08-23 22:26:23 +0800

    Make search input a controlled input

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ''
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({
+      term: event.target.value
+    });
+  }
+
   render() {
     return (
       <form className='search-form input-group'>
-        <input className='form-control' />
+        <input
+          placeholder='Get a five-day forecase in your favourite cities'
+          className='form-control'
+          value={this.state.term}
+          onChange={this.onInputChange}/>
         <span className='input-group-btn'>
           <button type='submit' className='btn btn-secondary'>Submit</button>
         </span>
