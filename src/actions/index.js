9b710031da56be45002a58888dc67fceb4782684
Shaii Ong <monsteronfire@gmail.com>
2017-08-24 21:00:49 +0800

    Add axios to handle ajax request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,17 @@
+import axios from 'axios';
 import config from '../config';
 
 const API_KEY = config.API_KEY;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appId=${API_KEY}`;
 
+export const FETCH_WEATHER = 'FETCH_WEATHER';
 
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
